6add7d78933fb9281c6e49f39e64207a4355a0eb
심성진-PC\심성진 <kumoh130715@gmail.com>
2018-03-20 20:50:45 +0900

    라즈베리파이 각 모듈들 구현
    버튼이 눌러져 있을 때 우유가 들어 있다고 가정하고, 초음파센서를 우유가방 옆에 설치하여 문이 열릴 때 벌어지는 거리차이를 이용하여 버저를 울리게 함.
    온습도 센서는 상할 가능성을 판별하기 위해 온도를 받아오고 일정 온도 이상일 경우 표시함

---
+++ milk.py
@@ -0,0 +1,60 @@
+﻿
+import time
+from grovepi import *
+import math
+import grovepi
+from time import sleep
+import os
+
+buzzer_pin = 2		#Port for buzzer
+button = 5		#Port for Button
+dht_sensor_port = 7 	#Port for dht_sensor
+dht_sensor_type = 0
+ultrasonic_ranger = 4	#Port for Ultrasonic_ranger
+
+distant =0
+down_flag=1			#button_flag
+temp_flag=1			#temp_flag
+
+pinMode(buzzer_pin,"OUTPUT")	# Assign mode for buzzer as output
+pinMode(button,"INPUT")		# Assign mode for Button as input
+while True:
+	try:
+		button_status= digitalRead(button)	#Read the Button status
+		distant = ultrasonicRead(ultrasonic_ranger)
+		if button_status:	#If the Button is in HIGH position, run the program
+			
+			if down_flag :
+				#button alarm
+				print("button push")
+				down_flag=0
+				str_sample='nodejs fcm-pushserver2.js m_on'
+				os.system(str_sample)
+				
+			[ temp,hum ] = dht(dht_sensor_port,dht_sensor_type)		
+			print("temp =", temp, "C\thumidity =",hum,"%", "distance=",distant)
+			if (temp>=22)&(temp_flag==1) :
+				print("temp= ", temp) 
+				temp_flag=0
+				sleep(0.5)
+				
+			if distant>=50 :
+				digitalWrite(buzzer_pin,1)
+				print("distance= ",distant)
+			else :
+				digitalWrite(buzzer_pin,0)
+			sleep(0.05)
+
+		else:		#If Button is in Off position, print "Off" on the screen
+			if down_flag==0 :
+				temp_flag=1
+				down_flag=1
+
+			digitalWrite(buzzer_pin,0)		
+	except KeyboardInterrupt:	# Stop the buzzer before stopping
+		print("key")
+		sleep(1)		
+		digitalWrite(buzzer_pin,0)
+		break
+	except (IOError,TypeError) as e:
+		print("Error")
